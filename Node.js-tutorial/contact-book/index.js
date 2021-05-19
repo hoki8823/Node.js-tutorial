119fca499a8d4b2f4ffd49967f9bf98366828e47
hoki8823 <hoki8823@naver.com>
2021-05-19 16:20:56 +0900

    주소록 - Show, Edit, Update, Destroy

--- Node.js-tutorial/contact-book/index.js
+++ Node.js-tutorial/contact-book/index.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 // DB setting
@@ -28,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 var contactSchema = mongoose.Schema({
   name:{type:String, required:true, unique:true},
@@ -54,6 +56,34 @@ app.get('/contacts/new',(req,res)=>{
 
 app.post('/contacts', function(req, res){
   Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+app.get('/contacts/:id',(req,res)=>{
+  Contact.findOne({_id:req.params.id},(err,contact)=>{
+    if(err) return res.json(err);
+    res.render('contacts/show',{contact:contact});
+  });
+});
+
+app.get('/contacts/:id/edit',(req,res)=>{
+  Contact.findOne({_id:req.params.id},(err,contact)=>{
+    if(err) return res.json(err);
+    res.render('contacts/edit',{contact:contact});
+  });
+});
+
+app.put('/contacts/:id',(req,res)=>{
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, (err,contact)=>{
+    if(err) return res.json(err);
+    res.redirect('/contacts/'+req.params.id);
+  });
+});
+
+app.delete('/contacts/:id',(req,res)=>{
+  Contact.deleteOne({_id:req.params.id},(err)=>{
     if(err) return res.json(err);
     res.redirect('/contacts');
   });
