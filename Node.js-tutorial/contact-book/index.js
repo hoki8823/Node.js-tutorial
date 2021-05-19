a0279e4f9f31592d695a0671d8746d14cbf1eee1
hoki8823 <hoki8823@naver.com>
2021-05-19 17:23:22 +0900

    주소록 - Module만들기

--- Node.js-tutorial/contact-book/index.js
+++ Node.js-tutorial/contact-book/index.js
@@ -31,63 +31,66 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
-var contactSchema = mongoose.Schema({
-  name:{type:String, required:true, unique:true},
-  email:{type:String},
-  phone:{type:String}
-});
+// var contactSchema = mongoose.Schema({
+//   name:{type:String, required:true, unique:true},
+//   email:{type:String},
+//   phone:{type:String}
+// });
 
-var Contact = mongoose.model('contact',contactSchema);
+// var Contact = mongoose.model('contact',contactSchema);
 
-app.get('/',(req,res)=>{
-  res.redirect('/contacts');
-});
+// app.get('/',(req,res)=>{
+//   res.redirect('/contacts');
+// });
 
-app.get('/contacts',(req,res)=>{
-  Contact.find({},(err,contacts)=>{
-    if(err) return res.json(err);
-    res.render('contacts/index',{contacts:contacts});
-  });
-});
+// app.get('/contacts',(req,res)=>{
+//   Contact.find({},(err,contacts)=>{
+//     if(err) return res.json(err);
+//     res.render('contacts/index',{contacts:contacts});
+//   });
+// });
 
-app.get('/contacts/new',(req,res)=>{
-  res.render('contacts/new');
-});
+// app.get('/contacts/new',(req,res)=>{
+//   res.render('contacts/new');
+// });
 
-app.post('/contacts', function(req, res){
-  Contact.create(req.body, function(err, contact){
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
+// app.post('/contacts', function(req, res){
+//   Contact.create(req.body, function(err, contact){
+//     if(err) return res.json(err);
+//     res.redirect('/contacts');
+//   });
+// });
 
-app.get('/contacts/:id',(req,res)=>{
-  Contact.findOne({_id:req.params.id},(err,contact)=>{
-    if(err) return res.json(err);
-    res.render('contacts/show',{contact:contact});
-  });
-});
+// app.get('/contacts/:id',(req,res)=>{
+//   Contact.findOne({_id:req.params.id},(err,contact)=>{
+//     if(err) return res.json(err);
+//     res.render('contacts/show',{contact:contact});
+//   });
+// });
 
-app.get('/contacts/:id/edit',(req,res)=>{
-  Contact.findOne({_id:req.params.id},(err,contact)=>{
-    if(err) return res.json(err);
-    res.render('contacts/edit',{contact:contact});
-  });
-});
+// app.get('/contacts/:id/edit',(req,res)=>{
+//   Contact.findOne({_id:req.params.id},(err,contact)=>{
+//     if(err) return res.json(err);
+//     res.render('contacts/edit',{contact:contact});
+//   });
+// });
 
-app.put('/contacts/:id',(req,res)=>{
-  Contact.findOneAndUpdate({_id:req.params.id}, req.body, (err,contact)=>{
-    if(err) return res.json(err);
-    res.redirect('/contacts/'+req.params.id);
-  });
-});
+// app.put('/contacts/:id',(req,res)=>{
+//   Contact.findOneAndUpdate({_id:req.params.id}, req.body, (err,contact)=>{
+//     if(err) return res.json(err);
+//     res.redirect('/contacts/'+req.params.id);
+//   });
+// });
 
-app.delete('/contacts/:id',(req,res)=>{
-  Contact.deleteOne({_id:req.params.id},(err)=>{
-    if(err) return res.json(err);
-    res.redirect('/contacts');
-  });
-});
+// app.delete('/contacts/:id',(req,res)=>{
+//   Contact.deleteOne({_id:req.params.id},(err)=>{
+//     if(err) return res.json(err);
+//     res.redirect('/contacts');
+//   });
+// });
+
+app.use('/',require('./routes/home'));
+app.use('/contacts',require('./routes/contacts'));
 
 // Port setting
 app.listen(52273, function(){
